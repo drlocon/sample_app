549e436d872c3c6dec9bd8ebaef8d98c497525d7
drlocon <104292397+drlocon@users.noreply.github.com>
2022-05-02 07:55:57 +0000

    [Finish]3-2

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,33 @@
 class ListsController < ApplicationController
   def new
+    #Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する
+    @list = List.new
+  end
+
+  #2章 保存機能を追加
+  def create
+    #データを受け取り、新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    #データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    #トップ画面へリダイレクト
+    redirect_to '/top'
   end
 
   def index
+    @lists = List.all
   end
 
   def show
   end
 
   def edit
+  end
+
+  #2章 private ここから下は、このcontrollerの中でしか呼び出せない
+  private
+  #2章 ストロングパラメータ(フォームからデータを送信する際の脆弱性を防ぐ)
+  def list_params
+    params.require(:list).permit(:title,:body)
   end
 end
